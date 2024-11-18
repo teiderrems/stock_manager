9999d9e6e4fbdffd6e7d5b7efd213f3998402602
teiderrems <raoul.teida@facsciences-uy1.cm>
2024-11-18 03:57:23 +0100

    add responseBody and change connection string

---
+++ backend/Dto/ResponseBody.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Dto
+{
+    public class ResponseBody(bool s, string[] error)
+    {
+        public bool Succeded {  get; set; }=s;
+        public string[] Error { get; set; } = error;
+    }
+}
